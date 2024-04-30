6422d510d5b4a026c3c582c91d95ffc5fb0a7620
arosas09 <arosas23001@gmail.com>
2024-04-30 11:21:29 -0700

    Added final things
    added difficulty buttons and changed background color and finished.

--- Unit5RosasP3/Assets/Scripts/GameManager.cs
+++ Unit5RosasP3/Assets/Scripts/GameManager.cs
@@ -15,15 +15,12 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI GameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-
-        score = 0;
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+       
     }
 
     // Update is called once per frame
@@ -60,5 +57,18 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame (int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+  
     }
 }
